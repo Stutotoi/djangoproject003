c05c6e381b79055605ee9d25c24f0f743535ac75
luis salazar <escor.g39@gmail.com>
2025-05-12 14:49:43 -0400

    sabe

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from .views import home, product_detail
 
 urlpatterns = [
     path('', home, name='home'),
+]
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 def home(request):
     products = Product.objects.all()[:3]  # Mostramos los primeros 3 productos
     return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'store/product_detail.html', {'product': product})
